78e3f0def51f1753a6923620553e90ef76698f79
Marwen JAIEM <marwen.jaiem@gmail.com>
2025-06-17 01:19:13 +0200

    ajout de tailwind Css, et config pour optimisation des performances du pc en dev

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  server: {
+    watch: {
+      // Ignore les gros dossiers inutiles
+      ignored: ['**/node_modules/**', '**/.git/**', '**/.vite/**'],
+    },
+    fs: {
+      // Empêche Vite de scanner hors du dossier projet
+      strict: true,
+      allow: ['.'] // ou ['.'] si tu veux autoriser tout le projet
+    }
+  }
+});
